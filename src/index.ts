6a04a3a253580a1cbcccab780e75fc433dbf0c71
Seunghyun Min <i@seunghyun.in>
2023-04-03 21:55:22 +0900

    fix: cron scheduling mistake

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,23 @@ import dotenv from 'dotenv';
 import { app } from './app';
 import { MenuNotificationService } from './services/menu-notification/menu-notification.service';
 import * as cron from 'node-cron';
-import { MENU_CHANNEL_ID } from './utils/consts';
+import { MENU_CHANNEL_ID, TEST_CHANNEL_ID } from './utils/consts';
 
 dotenv.config();
 
-const menuNotificationService = new MenuNotificationService(MENU_CHANNEL_ID);
+const menuNotificationService = new MenuNotificationService(TEST_CHANNEL_ID);
 
 app.event('app_mention', async ({ event, say }) => {
   await say(`Hey there <@${event.user}>!`);
 });
 
-cron.schedule('30-30 11-13 * * MON-FRI', async () => {
+cron.schedule('30-59 11 * * MON-FRI', async () => {
+  await menuNotificationService.sendMenuNotification();
+});
+cron.schedule('* 12 * * MON-FRI', async () => {
+  await menuNotificationService.sendMenuNotification();
+});
+cron.schedule('0-30 13 * * MON-FRI', async () => {
   await menuNotificationService.sendMenuNotification();
 });
 
