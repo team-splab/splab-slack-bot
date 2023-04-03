0b7a2947a2ffb4b60fe82b2ab0dbef4b63c6727c
Seunghyun Min <i@seunghyun.in>
2023-04-03 23:49:30 +0900

    fix: cron timezone

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,33 @@ import { MENU_CHANNEL_ID, TEST_CHANNEL_ID } from './utils/consts';
 
 dotenv.config();
 
-const menuNotificationService = new MenuNotificationService(TEST_CHANNEL_ID);
+const menuNotificationService = new MenuNotificationService(MENU_CHANNEL_ID);
 
 app.event('app_mention', async ({ event, say }) => {
   await say(`Hey there <@${event.user}>!`);
 });
 
-cron.schedule('30-59 11 * * MON-FRI', async () => {
-  await menuNotificationService.sendMenuNotification();
-});
-cron.schedule('* 12 * * MON-FRI', async () => {
-  await menuNotificationService.sendMenuNotification();
-});
-cron.schedule('0-30 13 * * MON-FRI', async () => {
-  await menuNotificationService.sendMenuNotification();
-});
+cron.schedule(
+  '30-59 11 * * MON-FRI',
+  async () => {
+    await menuNotificationService.sendMenuNotification();
+  },
+  { timezone: 'Asia/Seoul' }
+);
+cron.schedule(
+  '* 12 * * MON-FRI',
+  async () => {
+    await menuNotificationService.sendMenuNotification();
+  },
+  { timezone: 'Asia/Seoul' }
+);
+cron.schedule(
+  '0-30 13 * * MON-FRI',
+  async () => {
+    await menuNotificationService.sendMenuNotification();
+  },
+  { timezone: 'Asia/Seoul' }
+);
 
 (async () => {
   await app.start();
