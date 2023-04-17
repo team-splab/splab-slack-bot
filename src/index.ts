de2673d1ebc4e47f61399b5027a18bd2447b5ebc
Seunghyun Min <i@seunghyun.in>
2023-04-17 10:41:04 +0900

    feat: change menu notification time

--- src/index.ts
+++ src/index.ts
@@ -13,27 +13,15 @@ app.event('app_mention', async ({ event, say }) => {
   console.log(`${new Date()} - App mentioned`);
 });
 
-cron.schedule(
-  '30-59/5 11 * * MON-FRI',
-  async () => {
-    await menuNotificationService.sendMenuNotification();
-  },
-  { timezone: 'Asia/Seoul' }
-);
-cron.schedule(
-  '*/5 12 * * MON-FRI',
-  async () => {
-    await menuNotificationService.sendMenuNotification();
-  },
-  { timezone: 'Asia/Seoul' }
-);
-cron.schedule(
-  '0-30/5 13 * * MON-FRI',
-  async () => {
-    await menuNotificationService.sendMenuNotification();
-  },
-  { timezone: 'Asia/Seoul' }
-);
+['*/5 9-12 * * MON-FRI', '0-30/5 13 * * MON-FRI'].forEach((cronTime) => {
+  cron.schedule(
+    cronTime,
+    async () => {
+      await menuNotificationService.sendMenuNotification();
+    },
+    { timezone: 'Asia/Seoul' }
+  );
+});
 
 (async () => {
   await app.start();
