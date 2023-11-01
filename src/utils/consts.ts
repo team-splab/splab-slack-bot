8f3a2a331423e0a44da0c1e1f47a97a960e2e962
Zin Park <zin@splab.dev>
2023-11-02 01:27:00 +0900

    feat: Space Daily Report

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,8 +1,12 @@
-export const TEST_CHANNEL_ID = 'C0502Q0S05P';
-export const MENU_CHANNEL_ID = 'C051U09FMBK';
+export const TEST_CHANNEL_ID = "C0502Q0S05P";
+export const MENU_CHANNEL_ID = "C051U09FMBK";
 
-export const DAYS_KOREAN = ['일', '월', '화', '수', '목', '금', '토'];
+export const DAYS_KOREAN = ["일", "월", "화", "수", "목", "금", "토"];
 
 export const ACTIONS = {
-  MENU_SELECT: 'menu_select',
+  MENU_SELECT: "menu_select",
+};
+
+export const SPACE = {
+  DAILY_REPORT: "/daily_report",
 };
