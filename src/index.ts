60174f72ca1e77eab5d62e0566874b9517212987
Seunghyun Min <i@seunghyun.in>
2023-11-02 10:26:38 +0900

    Revert "feat: Space Daily Report"
    This reverts commit 8f3a2a331423e0a44da0c1e1f47a97a960e2e962.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import dotenv from "dotenv";
-import { app } from "./app";
-import { MenuNotificationService } from "./services/menu-notification/menu-notification.service";
-import * as cron from "node-cron";
-import { ACTIONS, MENU_CHANNEL_ID } from "./utils/consts";
-import { SPACE } from "./utils/consts";
-import { MenuSelectService } from "./services/menu-notification/menu-select.service";
-import { DailyReportService } from "./services/space/daily-report/daily-report.service";
+import dotenv from 'dotenv';
+import { app } from './app';
+import { MenuNotificationService } from './services/menu-notification/menu-notification.service';
+import * as cron from 'node-cron';
+import { ACTIONS, MENU_CHANNEL_ID } from './utils/consts';
+import { MenuSelectService } from './services/menu-notification/menu-select.service';
 
 dotenv.config();
 
@@ -13,28 +11,25 @@ export const menuNotificationService = new MenuNotificationService(
   MENU_CHANNEL_ID
 );
 const menuSelectService = new MenuSelectService();
-const dailyReportService = new DailyReportService();
 
-app.event("app_mention", async ({ event, say }) => {
+app.event('app_mention', async ({ event, say }) => {
   console.log(`${new Date()} - App mentioned`);
   await say(`Hey there <@${event.user}>! ${new Date().toLocaleString()}`);
 });
 
 app.action(ACTIONS.MENU_SELECT, menuSelectService.onMenuSelectAction);
 
-app.command(SPACE.DAILY_REPORT, dailyReportService.sendDailyReport);
-
-["*/5 9-12 * * MON-FRI", "0-30/5 13 * * MON-FRI"].forEach((cronTime) => {
+['*/5 9-12 * * MON-FRI', '0-30/5 13 * * MON-FRI'].forEach((cronTime) => {
   cron.schedule(
     cronTime,
     async () => {
       await menuNotificationService.sendMenuNotification();
     },
-    { timezone: "Asia/Seoul" }
+    { timezone: 'Asia/Seoul' }
   );
 });
 
 (async () => {
   await app.start();
-  console.log("⚡️ Bolt app started");
+  console.log('⚡️ Bolt app started');
 })();
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,12 +1,8 @@
-export const TEST_CHANNEL_ID = "C0502Q0S05P";
-export const MENU_CHANNEL_ID = "C051U09FMBK";
+export const TEST_CHANNEL_ID = 'C0502Q0S05P';
+export const MENU_CHANNEL_ID = 'C051U09FMBK';
 
-export const DAYS_KOREAN = ["일", "월", "화", "수", "목", "금", "토"];
+export const DAYS_KOREAN = ['일', '월', '화', '수', '목', '금', '토'];
 
 export const ACTIONS = {
-  MENU_SELECT: "menu_select",
-};
-
-export const SPACE = {
-  DAILY_REPORT: "/daily_report",
+  MENU_SELECT: 'menu_select',
 };
