cdf5dd1030600b36899624de3f6447bacf187a91
Seunghyun Min <i@seunghyun.in>
2024-01-03 10:39:29 +0900

    Remove menu notification services

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import dotenv from 'dotenv';
 import { app } from './app';
-import { MenuNotificationService } from './services/menu-notification/menu-notification.service';
 import { MENU_CHANNEL_ID, SLASH_COMMANDS } from './utils/consts';
-import { MenuSelectService } from './services/menu-notification/menu-select.service';
 import initUmohServices from './services/umoh';
 import { DailyReportService } from './services/umoh/daily-report/daily-report.service';
 import { HostManagementService } from './services/umoh/host/host-management.service';
 import { SpaceEditService } from './services/umoh/edit/space-edit.service';
 import { SlashCommandService } from './interfaces/slash-command-service';
-import initMenuNotificationService from './services/menu-notification';
 import { SpaceCategoryEditService } from './services/umoh/edit/space-category-edit.service';
 import { SpaceEditView } from './services/umoh/edit/space-edit.view';
 import { SpaceCategoryEditView } from './services/umoh/edit/space-category-edit.view';
@@ -19,10 +16,6 @@ import { CardCreateService } from './services/umoh/card-create/card-create.servi
 
 dotenv.config();
 
-export const menuNotificationService = new MenuNotificationService(
-  MENU_CHANNEL_ID
-);
-const menuSelectService = new MenuSelectService();
 const dailyReportService = new DailyReportService();
 const spaceEditView = new SpaceEditView();
 const spaceCategoryEditView = new SpaceCategoryEditView();
@@ -63,7 +56,6 @@ initUmohServices(
     (service) => service.slashCommandName === SLASH_COMMANDS.UMOH
   )
 );
-initMenuNotificationService(menuNotificationService, menuSelectService);
 
 (async () => {
   await redisClient.connect();
